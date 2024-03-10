a1556c0048bfa11a9064902f58ebcd5bf2bd4dcc
Nandy Bâ <nandyba@yahoo.fr>
2024-03-10 13:03:52 +0000

    feature: register Flare oracle

--- smart-contracts/src/WBTCReserve.sol
+++ smart-contracts/src/WBTCReserve.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IFtsoRegistry} from "lib/flare-foundry-periphery-package/src/coston2/ftso/userInterfaces/IFtsoRegistry.sol";
 
+import {FlareContractsRegistryLibrary} from "lib/flare-foundry-periphery-package/src/coston2/util-contracts/ContractRegistryLibrary.sol";
 contract WBTCReserve {
 
     IERC20 wbtc;
     mapping(address => uint256) public balances;
+    IFtsoRegistry ftsoRegistry;
     
     constructor(address _wBTC) {
         wbtc = IERC20(_wBTC);
@@ -29,6 +32,17 @@ contract WBTCReserve {
     }
 
     function getAssetPrice() external view returns (uint256) {
-        return 70000;
+        uint256 _price;
+        uint256 _timestamp;
+        uint256 _decimals;
+
+        (_price, _timestamp, _decimals) = ftsoRegistry
+            .getCurrentPriceWithDecimals("BTC");
+
+        return _price;
+    }
+
+    function registerOracle(address _ftsoRegistry) external {
+        ftsoRegistry = IFtsoRegistry(_ftsoRegistry);
     }
 }
